313e3aea4c892c4374ba255310a18facfa1811fd
Polandian <101973563+Polandian@users.noreply.github.com>
2023-04-03 00:16:31 +0300

    Update downloadlink.js

--- downloadlink.js
+++ downloadlink.js
@@ -1,3 +1,3 @@
 function filenewversiondownload(){
-  window.open('https://drive.google.com/file/d/1ewz8NFq4vjRxKPt-9LmPSq-Yr7a8uCWU/view', '_blank');
+  window.open('https://drive.google.com/uc?export=download&id=1ewz8NFq4vjRxKPt-9LmPSq-Yr7a8uCWU, '_blank');
 }
